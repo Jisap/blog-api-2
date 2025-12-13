012b280d7b5c56ac51ec84d8349604c4878663bf
Jorge Matos <jisapjisap@gmail.com>
2025-12-13 18:21:38 +0100

    feat: Implement user authentication with register and login routes, including input validation and token management.

---
+++ src/controllers/v1/auth/login.ts
@@ -0,0 +1,73 @@
+import { generateAccessToken, generateRefreshToken } from "@/lib/jwt";
+import { logger } from "@/lib/winston";
+import Token from "@/models/token";
+import User from "@/models/user";
+import config from "@/config";
+import type { Request, Response } from 'express';
+import type { IUser } from '@/models/user';
+
+
+type UserData = Pick<IUser, 'email' | 'password' | 'role'>;
+
+
+const login = async (req: Request, res: Response): Promise<void> => {
+
+  const { email } = req.body as UserData;                                      // Desestructuramos del body de la solicitud el email
+
+  try {
+
+    const user = await User.findOne({ email })                                   // Buscamos el usuario en la base de datos
+      .select('username email password role')
+      .lean()
+      .exec()
+
+    if (!user) {                                                                    // Si no se encuentra el usuario -> error 404
+      res.status(404).json({
+        code: 'NotFound',
+        message: 'User not found'
+      })
+      return;
+    }
+
+    const accessToken = generateAccessToken(user._id);                             // Generamos el accessToken
+    const refreshToken = generateRefreshToken(user._id);                           // Generamos el refreshToken
+
+    await Token.create({                                                           // Guardamos el refreshToken en la base de datos
+      token: refreshToken,
+      userId: user._id
+    });
+    logger.info('Refresh token created for user', {
+      userId: user._id,
+      token: refreshToken
+    })
+
+    res.cookie('refreshToken', refreshToken, {                                      // cookie en en los headers de la respuesta con el refreshToken
+      httpOnly: true,
+      secure: config.NODE_ENV === 'production',
+      sameSite: 'strict',
+    })
+
+    res.status(201).json({                                                          // user y accessToken en la respuesta de la solicitud
+      user: {
+        username: user.username,
+        email: user.email,
+        role: user.role,
+      },
+      accessToken
+    });
+
+    logger.info('User logged in successfully.', user)
+
+  } catch (error) {
+    res.status(500).json({
+      message: 'Internal server error',
+      error: error
+    })
+
+    logger.error('Error during user registration.', error)
+  }
+
+}
+
+export default login;
+
--- src/routes/v1/auth.ts
+++ src/routes/v1/auth.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import register from "@/controllers/v1/auth/register";
+import login from "@/controllers/v1/auth/login";
 import { body } from "express-validator";
 import validationError from "@/middlewares/validationError";
 import User from "@/models/user";
+import bcrypt from "bcrypt";
 
 
 const router = Router();
@@ -37,6 +39,48 @@ router.post(
     .default('user'),
   validationError,
   register
+);
+
+router.post(
+  '/login',
+  body('email') // validate email con express-validator
+    .trim()
+    .notEmpty()
+    .withMessage('Email is required')
+    .isLength({ max: 50 })
+    .withMessage('Email must be less than 50 characters long')
+    .isEmail()
+    .withMessage('Invalid email address')
+    .custom(async (value) => {
+      const userExists = await User.exists({ email: value }).lean();
+      if (!userExists) {
+        throw new Error('User Email or Password is invalid');
+      }
+    }),
+  body('password')
+    .notEmpty()
+    .withMessage('Password is required')
+    .isLength({ min: 8 })
+    .withMessage('Password must be at least 8 characters long')
+    .custom(async (value, { req }) => {                             // req.body -> email -> user -> password -> compare -> si no coincide -> error
+      const { email } = req.body as { email: string };
+      const user = await User.findOne({ email })
+        .select('password')
+        .lean()
+        .exec();
+
+      if (!user) {
+        throw new Error('User Email or Password is invalid');
+      }
+
+      const passwordMatch = await bcrypt.compare(value, user.password);
+      if (!passwordMatch) {
+        throw new Error('User Email or Password is invalid');
+      }
+
+    }),
+  validationError,
+  login
 )
 
 export default router;
