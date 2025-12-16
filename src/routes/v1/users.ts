e9bc85d37f294903f7f214516eb3b0e6bb9952b6
Jorge Matos <jisapjisap@gmail.com>
2025-12-16 16:21:13 +0100

    feat: Add API endpoint and controller for updating the current user's profile with comprehensive validation.

--- src/routes/v1/users.ts
+++ src/routes/v1/users.ts
@@ -1,4 +1,5 @@
 import getCurrentUser from "@/controllers/v1/user/get_current_user";
+import updateCurrentUser from "@/controllers/v1/user/update_current_user";
 import authenticate from "@/middlewares/authenticate";
 import authorize from "@/middlewares/authorize";
 import validationError from "@/middlewares/validationError";
@@ -16,5 +17,55 @@ router.get(
   authorize(['user', 'admin']),
   getCurrentUser
 );
+
+router.put(
+  '/current',
+  authenticate,
+  authorize(['user', 'admin']),
+  body('username')
+    .optional()
+    .trim()
+    .isLength({ max: 20 })
+    .withMessage('Username must be less than 20 characters long')
+    .custom(async (value) => {
+      const userExists = await user.exists({ username: value });
+      if (userExists) {
+        throw new Error('this username is already in use');
+      }
+    }),
+  body('email')
+    .optional()
+    .isLength({ max: 50 })
+    .withMessage('Email must be less than 50 characters long')
+    .isEmail()
+    .withMessage('Invalid email address')
+    .custom(async (value) => {
+      const userExists = await user.exists({ email: value });
+      if (userExists) {
+        throw new Error('this email is already in use');
+      }
+    }),
+  body('password')
+    .optional()
+    .isLength({ min: 8 })
+    .withMessage('Password must be at least 8 characters long'),
+  body('first_name')
+    .optional()
+    .isLength({ max: 20 })
+    .withMessage('First name must be less than 20 characters long'),
+  body('last_name')
+    .optional()
+    .trim()
+    .isLength({ max: 20 })
+    .withMessage('Last name must be less than 20 characters long'),
+  body(['website', 'facebook', 'x', 'instagram', 'linkedin', 'youtube'])
+    .optional()
+    .isURL()
+    .withMessage('Invalid URL')
+    .isLength({ max: 100 })
+    .withMessage('URL must be less than 100 characters long'),
+  validationError,
+  updateCurrentUser
+)
 
 export default router;
