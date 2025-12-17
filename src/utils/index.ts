691b8886594da6c26b01ba8e3d115cb7743e5b48
Jorge Matos <jisapjisap@gmail.com>
2025-12-17 18:12:36 +0100

    feat: add utility functions for generating usernames and unique slugs.

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -8,3 +8,18 @@ export const genUsername = (email: string): string => {
   return username;
 }
 
+export const genSlug = (title: string): string => {
+
+  const slug = title
+    .toLowerCase()
+    .trim()
+    .replace(/[^a-z0-9]\s-/g, "-") // Reemplaza caracteres no permitidos por guiones
+    .replace(/\s+/g, "-")          // Reemplaza espacios por guiones
+    .replace(/-+/g, "-")           // Reemplaza múltiples guiones por uno solo
+
+  const randomChars = Math.random().toString(36).slice(2); // Genera caracteres aleatorios
+  const uniqueSlug = `${slug}-${randomChars}`;             // Combina el slug original con los caracteres aleatorios
+
+
+  return uniqueSlug;
+}
