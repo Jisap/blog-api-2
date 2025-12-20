e67904830cdaa87335bb7aa2e03ea055050d8901
Jorge Matos <jisapjisap@gmail.com>
2025-12-20 16:11:30 +0100

    feat: Add API endpoint and controller to retrieve comments for a specific blog.

---
+++ src/controllers/v1/comment/get_comments_by_blog.ts
@@ -0,0 +1,37 @@
+import { logger } from '@/lib/winston';
+import type { Request, Response } from 'express';
+import Blog from '@/models/Blog';
+import Comment from '@/models/comment';
+import type { IComment } from '@/models/comment';
+
+
+
+const getCommentsByBlog = async (req: Request, res: Response) => {
+
+  const { blogId } = req.params;
+
+  try {
+    const blog = await Blog.findById(blogId).select('_id').lean().exec();
+    if (!blog) {
+      return res.status(404).json({
+        code: "BlogNotFound",
+        message: "Blog not found"
+      });
+    }
+
+    const allComments = await Comment.find({ blogId }).sort({ createdAt: -1 }).lean().exec();
+
+    return res.status(201).json({
+      allComments
+    });
+
+  } catch (error) {
+    logger.error("Error while getting comments by blog.", error);
+    return res.status(500).json({
+      code: "ServerError",
+      message: "Internal server error"
+    });
+  }
+}
+
+export default getCommentsByBlog
--- src/routes/v1/comment.ts
+++ src/routes/v1/comment.ts
@@ -6,6 +6,7 @@ import authorize from "@/middlewares/authorize";
 import validationError from "@/middlewares/validationError";
 import unlikeBlog from "@/controllers/v1/like/unlike_blog";
 import commentBlog from "@/controllers/v1/comment/comment_blog";
+import getCommentsByBlog from "@/controllers/v1/comment/get_comments_by_blog";
 
 
 const router = Router();
@@ -23,6 +24,17 @@ router.post(
     .withMessage('Content is required'),
   validationError,
   commentBlog
+);
+
+router.get(
+  '/blog/:blogId',
+  authenticate,
+  authorize(['admin', 'user']),
+  param('blogId')
+    .isMongoId()
+    .withMessage('Invalid blog ID'),
+  validationError,
+  getCommentsByBlog
 );
 
 export default router;
